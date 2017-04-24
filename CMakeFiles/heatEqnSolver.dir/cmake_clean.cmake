a47c1e3a2f4f7c16774bd4954da5536f8c432498
prince-ph0en1x <aritra44sarkar@yahoo.co.in>
2017-04-24 19:25:12 +0100

    dummy compile cmake

---
+++ CMakeFiles/heatEqnSolver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/heatEqnSolver.dir/src/heatEqnSolver.cxx.o"
+  "heatEqnSolver.pdb"
+  "heatEqnSolver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/heatEqnSolver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
